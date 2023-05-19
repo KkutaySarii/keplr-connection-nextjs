cac4034be6ecdf301cda096e6c32661be8c65753
KkutaySarii <h.kutay.1@gmail.com>
2023-05-19 16:11:37 +0300

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,30 @@
-import '@/styles/globals.css'
+import { WalletProvider } from "@/context/wallet";
+import { useKeplr } from "@/services/keplr";
+import "@/styles/globals.css";
+import { useEffect } from "react";
+
+const SideEffects = () => {
+  const keplr = useKeplr();
+
+  useEffect(() => {
+    const listenKeystoreChange = () => keplr.connect(true);
+    window.addEventListener("keplr_keystorechange", listenKeystoreChange);
+  }, [keplr]);
+
+  useEffect(() => {
+    const walletAddress = window.localStorage.getItem("wallet_address");
+    if (walletAddress) {
+      keplr.connect();
+    }
+  }, [keplr]);
+  return null;
+};
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <WalletProvider>
+      <SideEffects />
+      <Component {...pageProps} />
+    </WalletProvider>
+  );
 }
